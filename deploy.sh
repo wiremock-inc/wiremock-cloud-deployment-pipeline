197f2442734f0d08dfd70e659d0c0e106114217b
Robert Elliot <rob@lidalia.org.uk>
2022-05-24 13:02:18 +0100

    Start using the actual cdk docker image

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,10 @@ do_deploy() {
   local cdk_image=$1
   local stack=$2
 
-  echo "ENV_SPECIFIC_SECRET = [$ENV_SPECIFIC_SECRET]"
-  echo docker run --rm \
-      --mount type=bind,src=imagedefinitions.json,target=/etc/imagedefinitions.json \
+  docker run --rm \
+      --mount "type=bind,src=${PWD}/imagedefinitions.json,target=/etc/imagedefinitions.json" \
       "$cdk_image" \
-      "$stack"
+      list
 }
 
 main "$@"
