a8b9ef31c76bb2ca38d4ddb757544f9b1918cb6b
Robert Elliot <rob@wiremock.io>
2023-01-26 17:30:15 +0000

    Support pushing arbitrary branches to dev

--- set-version.sh
+++ set-version.sh
@@ -2,22 +2,42 @@
 
 set -euo pipefail
 
+integer="(0|[1-9][0-9]*)"
+qualifier="[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*"
+semver_regex="^$integer\.$integer\.$integer(\-$qualifier)?(\+$qualifier)?$"
+
 main() {
   local name=$1
   local image_uri=$2
   local source_action=$3
   local source_commit=$4
+  local force=$5
 
-  alter_imagedefinitions "$name" imageUri "$image_uri"
-  alter_imagedefinitions "$name" metadata.sourceAction "$source_action"
-  alter_imagedefinitions "$name" metadata.sourceCommit "$source_commit"
+  local update
+  local current_tag; current_tag="$(find_current_tag "$name")"
+  echo "Current tag: $current_tag force: $force"
 
-  git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-  git config --local user.name "github-actions[bot]"
+  if "$force" || [[ "$current_tag" =~ $semver_regex ]]; then
+    echo "Setting the version"
+    update=true
+  else
+    echo "Not setting the version"
+    update=false
+  fi
 
-  git add imagedefinitions.json
-  git commit -m "Set $name to ${image_uri#*:}" -m "$image_uri"
-  git push
+  if $update; then
+
+    alter_imagedefinitions "$name" imageUri "$image_uri"
+    alter_imagedefinitions "$name" metadata.sourceAction "$source_action"
+    alter_imagedefinitions "$name" metadata.sourceCommit "$source_commit"
+
+    git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+    git config --local user.name "github-actions[bot]"
+
+    git add imagedefinitions.json
+    git commit -m "Set $name to ${image_uri##*:}" -m "$image_uri"
+    git push
+  fi
 }
 
 alter_imagedefinitions() {
@@ -28,6 +48,13 @@ alter_imagedefinitions() {
   local work_file; work_file=$(mktemp "/tmp/imagedefinitions.json.XXXXXX")
   jq '( .[] | select(.name == "'"$name"'") ).'"$key"' = "'"$value"'"' imagedefinitions.json > "$work_file"
   mv "$work_file" imagedefinitions.json
+}
+
+find_current_tag() {
+  local name=$1
+
+  local currentImageName; currentImageName=$(jq -r '( .[] | select(.name == "'"$name"'") ).imageUri' imagedefinitions.json)
+  echo "${currentImageName##*:}"
 }
 
 main "$@"
