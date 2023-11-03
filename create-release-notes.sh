9ecd42c058101c3b44b915e891820f96e01a026b
Robert Elliot <rob@wiremock.io>
2023-11-03 22:07:14 +0000

    Better logging

--- create-release-notes.sh
+++ create-release-notes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -exuo pipefail
+set -euo pipefail
 
 main() {
 
@@ -78,6 +78,7 @@ get_release_notes() {
     rm -rf /tmp/release_notes
     mkdir /tmp/release_notes
     cd /tmp/release_notes
+    echo "Cloning git@github.com:$repo.git"
     git clone --filter=blob:none --no-checkout --depth 200 --single-branch --branch "$branch" "git@github.com:$repo.git" -q .
 
     echo "[$name](https://github.com/$repo/compare/$start..$end) [$start](https://github.com/$repo/releases/tag/$start) -> [$end](https://github.com/$repo/releases/tag/$end)"
